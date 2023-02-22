bb74f914532dbdc466a1ba80c2cb350547868ffb
Zielony Trójkąt <lemonlemongrab1@gmail.com>
2023-02-22 16:46:40 +0100

    fixed attributes yay

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,3 +1,7 @@
 Events.on(ContentInitEvent, e => {
-	 Vars.content.block("glacier-wall").attributes.set(Attribute.get("ice"), 1);
-	 Vars.content.block("bauxite-wall").attributes.set(Attribute.get("bauxite"), 0.25);
+     Vars.content.block("glacier-wall").attributes.set(Attribute.get("ice"), 1);
+     Vars.content.block("bauxite-wall").attributes.set(Attribute.get("bauxite"), 0.25);
+
+     Vars.content.block("bauxite-crusher").attribute = Attribute.get("bauxite");
+     Vars.content.block("ice-crusher").attribute = Attribute.get("ice");
+});
